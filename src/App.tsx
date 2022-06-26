07adef62e6415db4380b4c3e4e29978f4193cc7e
saintic <saintic132@gmail.com>
2022-06-27 01:52:12 +0300

    Переформатирован код, добавлены дополнительные проверки для номера телефона и сообщений

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,96 @@ import style from './App.module.scss'
 type FormData = {
     name: string
     email: string
-    phoneNumber: string
+    phone: string
     birthday: string
     message: string
 }
 
 function App() {
-    const [formData, setFormData] = useState({
+
+    const [formData, setFormData] = useState<FormData>({
         name: '',
         email: '',
-        phoneNumber: '',
+        phone: '',
         birthday: '',
         message: '',
     })
 
-    const [errors, setErrors] = useState<{ [K in keyof FormData]?: string } | null>(null)
+    const [errors, setErrors] = useState<{ [K in keyof FormData]?: string | number }>({
+        name: '',
+        email: '',
+        phone: '',
+        message: ''
+    })
 
-    const setFormDataHandler = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
-        setFormData({
-            ...formData,
-            [e.currentTarget.dataset.field as string]:
-                e.currentTarget.dataset.field === 'name'
-                    ? e.currentTarget.value.toUpperCase()
-                    : e.currentTarget.value
-        })
+    const helpValidator = (value: string, field: string) => {
+        value
+            ? validatorFormData(field, value)
+            : setErrors({...errors, [field]: ''})
+    }
+
+    const formattedPhoneNumber = (value: string, selectValue: number | null) => {
+        let inputValue = value.replace(/\D/g, '')
+        let formattedInput = ''
+
+        if (inputValue.length > 10) {
+            return formData.phone
+        }
+        if (value.length !== selectValue) {
+            return value
+        }
+        if (inputValue.length === 1) {
+            formattedInput += '(' + inputValue
+        }
+        if (inputValue.length > 1) {
+            formattedInput += '(' + inputValue.substring(0, 3)
+        }
+        if (inputValue.length >= 4) {
+            formattedInput += ') ' + inputValue.substring(3, 6)
+        }
+        if (inputValue.length >= 7) {
+            formattedInput += '-' + inputValue.substring(6, 8)
+        }
+        if (inputValue.length >= 9) {
+            formattedInput += '-' + inputValue.substring(8, 10)
+        }
+        return formattedInput
+    }
+
+    const setFormDataHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         switch (e.currentTarget.dataset.field) {
             case 'name': {
-                e.currentTarget.value
-                    ? validatorFormData(e.currentTarget.dataset.field, e.currentTarget.value)
-                    // @ts-ignore
-                    : setErrors(delete errors['name'])
+                helpValidator(e.currentTarget.value, e.currentTarget.dataset.field)
+                setFormData({
+                    ...formData,
+                    [e.currentTarget.dataset.field]: e.currentTarget.value.toUpperCase()
+                })
                 break;
             }
             case 'email': {
-                e.currentTarget.value
-                    ? validatorFormData(e.currentTarget.dataset.field, e.currentTarget.value)
+                helpValidator(e.currentTarget.value, e.currentTarget.dataset.field)
+                setFormData({
+                    ...formData,
+                    [e.currentTarget.dataset.field]: e.currentTarget.value
+                })
+                break;
+            }
+            case 'phone': {
+                helpValidator(e.currentTarget.value, e.currentTarget.dataset.field)
+                setFormData({
+                    ...formData,
                     // @ts-ignore
-                    : setErrors(delete errors['email'])
+                    [e.currentTarget.dataset.field]: formattedPhoneNumber(e.currentTarget.value, e.currentTarget.selectionStart)
+                })
+
                 break;
             }
             case 'message': {
-                e.currentTarget.value
-                    ? validatorFormData(e.currentTarget.dataset.field, e.currentTarget.value)
-                    // @ts-ignore
-                    : setErrors(delete errors['message'])
+                helpValidator(e.currentTarget.value, e.currentTarget.dataset.field)
+                setFormData({
+                    ...formData,
+                    [e.currentTarget.dataset.field]: e.currentTarget.value
+                })
                 break;
             }
         }
@@ -57,32 +103,40 @@ function App() {
         switch (dataField) {
             case 'name': {
                 if (!/^[a-z]{3,30} [a-z]{3,30}$/i.test(value)) {
-                    const error = {name: 'First and Last name min 3 and max 30 letters'}
-                    const newErrors = errors ? Object.assign(errors, error) : error
-                    setErrors(newErrors)
+                    const error = 'First and Last name min 3 and max 30 letters'
+                    setErrors({...errors, name: error})
+                } else {
+                    setErrors({...errors, name: ''})
                 }
                 break;
             }
             case 'email': {
                 if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-                    const error = {email: 'Invalid email address'}
-                    const newErrors = errors ? Object.assign(errors, error) : error
-                    setErrors(newErrors)
+                    const error = 'Invalid email address'
+                    setErrors({...errors, email: error})
+                } else {
+                    setErrors({...errors, email: ''})
+                }
+                break;
+            }
+            case 'phone': {
+                if (!/^[(]\d{3}[)]\s\d{3}[-]\d{2}[-]\d{2}/g.test(value)) {
+                    const error = 'Accepted only numeric number, e.g. +7 (495) 555 55 55'
+                    setErrors({...errors, phone: error})
+                } else {
+                    setErrors({...errors, phone: ''})
                 }
                 break;
             }
             case 'message': {
-                if (value.length < 10) {
-                    const error = {message: 'Min 10 letters'}
-                    const newErrors = errors ? Object.assign(errors, error) : error
-                    setErrors(newErrors)
+                if (value.length > 0 && value.length < 10) {
+                    const error = 'Min 10 letters'
+                    setErrors({...errors, message: error})
                 } else if (value.length === 300) {
-                    const error = {message: 'Max 300 letters'}
-                    const newErrors = errors ? Object.assign(errors, error) : error
-                    setErrors(newErrors)
+                    const error = 'Max 300 letters'
+                    setErrors({...errors, message: error})
                 } else {
-                    // @ts-ignore
-                    setErrors(delete errors['message'])
+                    setErrors({...errors, message: ''})
                 }
                 break;
             }
@@ -101,10 +155,11 @@ function App() {
                             value={formData.name}
                             data-field='name'
                             onChange={setFormDataHandler}
+                            placeholder='e.g. "BARBARA LISKOV"'
                         />
 
                         {
-                            errors?.name
+                            errors.name
                                 ? <div className={style.error}>{errors.name}</div>
                                 : <div className={style.fakeDiv}/>
                         }
@@ -117,6 +172,8 @@ function App() {
                             value={formData.email}
                             data-field='email'
                             onChange={setFormDataHandler}
+                            autoComplete='none'
+                            placeholder='Enter your email'
                         />
 
                         {
@@ -129,11 +186,26 @@ function App() {
                     <div className={style.app__fields_column}>
                         <label>Phone number</label>
                         <input
+                            className={style.input__phone}
                             type="tel"
-                            value={formData.phoneNumber}
+                            value={formData.phone}
                             data-field='phone'
                             onChange={setFormDataHandler}
+                            placeholder='(495) 555-55-55'
                         />
+
+                        {
+                            <div className={style.phoneFormat}>
+                                {'+7'}
+                            </div>
+                        }
+
+                        {
+                            errors?.phone
+                                ? <div className={style.error}>{errors.phone}</div>
+                                : <div className={style.fakeDiv}/>
+                        }
+
                     </div>
                     <div className={style.app__fields_column}>
                         <label>Birthday</label>
@@ -148,7 +220,6 @@ function App() {
                         <label>Message</label>
                         <textarea
                             value={formData.message}
-                            required
                             minLength={10}
                             maxLength={300}
                             data-field='message'
